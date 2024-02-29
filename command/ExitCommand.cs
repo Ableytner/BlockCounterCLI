69799016b63fa7225f584fd668cf0fcc03139ea2
Ableytner <ableit190030@sr.htlweiz.at>
2024-02-29 21:25:32 +0100

    add ExitCommand

---
+++ command/ExitCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace BlockCounterCLI.command
+{
+    internal class ExitCommand : BaseCommand
+    {
+        public static new string prefix = "exit";
+        public static new string description = "stops the program";
+
+        public ExitCommand(CommandRegistry commandRegistry)
+        {
+            this.commandRegistry = commandRegistry;
+        }
+
+        protected CommandRegistry commandRegistry;
+
+        public override void Execute()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
