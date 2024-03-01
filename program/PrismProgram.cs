1445050e7a188b9c0dad8973feb7f3d3a092729a
Ableytner <ableit190030@sr.htlweiz.at>
2024-03-01 18:21:40 +0100

    automatically create instance

--- program/PrismProgram.cs
+++ program/PrismProgram.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,19 +23,29 @@ namespace BlockCounterCLI.helpers
 
         public override void Setup()
         {
+            SetupLauncher();
+            SetupLauncherConfig();
+            SetupInstance();
+        }
+
+        private void SetupLauncher()
+        {
             // download files
             string url = "https://github.com/PrismLauncher/PrismLauncher/releases/download/8.0/PrismLauncher-Windows-MSVC-Portable-8.0.zip";
             string prism_zip = FileHelper.DownloadFile(url);
 
-            url = "https://raw.githubusercontent.com/Ableytner/BlockCounterCLI/main/data/accounts.json";
+            // copy files
+            string prism_dir = FileHelper.GetProgramsPath("prism");
+            FileHelper.UnzipFile(prism_zip, prism_dir);
+        }
+
+        private void SetupLauncherConfig()
+        {
+            string url = "https://raw.githubusercontent.com/Ableytner/BlockCounterCLI/main/data/accounts.json";
             string prism_accounts_dl = FileHelper.DownloadFile(url);
 
             url = "https://raw.githubusercontent.com/Ableytner/BlockCounterCLI/main/data/prismlauncher.cfg";
             string prism_config_dl = FileHelper.DownloadFile(url);
-
-            // copy files
-            string prism_dir = FileHelper.GetProgramsPath("prism");
-            FileHelper.UnzipFile(prism_zip, prism_dir);
 
             string prism_accounts = Path.Combine(FileHelper.GetProgramsPath("prism"), "accounts.json");
             FileHelper.CopyFile(prism_accounts_dl, prism_accounts);
@@ -47,6 +58,21 @@ namespace BlockCounterCLI.helpers
             config_content = config_content.Replace("[hostname_here]", Environment.MachineName);
             File.WriteAllText(prism_config_dl, config_content);
             FileHelper.CopyFile(prism_config_dl, prism_config);
+        }
+
+        private void SetupInstance()
+        {
+            string url = "https://raw.githubusercontent.com/Ableytner/BlockCounterCLI/main/data/Forge1.7.10Template/instance.cfg";
+            string instance_config = FileHelper.DownloadFile(url);
+
+            url = "https://raw.githubusercontent.com/Ableytner/BlockCounterCLI/main/data/Forge1.7.10Template/mmc-pack.json";
+            string mmc_pack = FileHelper.DownloadFile(url);
+
+            string instance_dir = Path.Combine(FileHelper.GetProgramsPath("prism"), "instances", "Forge1.7.10template");
+            Directory.CreateDirectory(instance_dir);
+
+            FileHelper.CopyFile(instance_config, Path.Combine(instance_dir, "instance.cfg"));
+            FileHelper.CopyFile(mmc_pack, Path.Combine(instance_dir, "mmc-pack.json"));
         }
     }
 }
